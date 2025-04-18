a68188ad5f80d46ee0dba5da37281c7e24750f59
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2025-04-18 11:14:01 +0500

    commit 9

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\ShortUrlController;
+
+Route::post('/shorten', [ShortUrlController::class, 'store']);
+
+Route::get('/shorten/{code}', [ShortUrlController::class, 'show']);
+
+Route::put('/shorten/{code}', [ShortUrlController::class, 'update']);
+
+Route::delete('/shorten/{code}', [ShortUrlController::class, 'destroy']);
+
+Route::get('/shorten/{code}/stats', [ShortUrlController::class, 'stats']);
